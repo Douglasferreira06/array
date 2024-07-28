5e11f31a9a0807167cddd02e8df4bd5cf7b74eec
email aluno2165@gmail.com <98502897+Douglasferreira06@users.noreply.github.com>
2024-07-28 10:50:58 -0300

    array
    array

---
+++ script.js
@@ -0,0 +1,47 @@
+
+
+
+/*function inserirAluno(nome, idade){
+
+        return {nome,idade}
+}
+
+let alunos = [
+
+
+    inserirAluno("douglas", 35),
+    inserirAluno("vanusa",26),
+    inserirAluno("frederico",3),
+    inserirAluno("angelita",76),
+]
+
+for (let aluno of alunos ){
+    if (aluno.idade <= 30){
+        console.log(aluno.nome)
+    }
+}*/
+
+/*function inserirAluno(nome, idade) {
+    return { nome: nome, idade: idade };
+}
+
+let alunos = [
+    inserirAluno("douglas", 35),
+    inserirAluno("vanusa", 26),
+    inserirAluno("frederico", 3),
+    inserirAluno("angelita", 76),
+];
+
+function nomeIdade(aluno){
+    return aluno.nome + " tem " + aluno.idade +" *anos "
+}
+console.log(alunos.map(nomeIdade));*/
+
+let turmaA =["maria","jose","paulo"]
+
+let turmaB = turmaA;
+
+turmaB.push(" romário ","  vanusa cu de burro ")
+
+
+console.log(turmaA)
